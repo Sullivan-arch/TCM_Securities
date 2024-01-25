a98e682ecf670c762f9c3491c1c3c275e9f20657
Sullivan <sullivanabegg8@gmail.com>
2024-01-25 09:01:27 -0500

    Strings

---
+++ Strings/first.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Programmer: Sullivan Abegg
+#Program: Strings
+
+print("Detroit Pistons are winning the Championship\n")
+print("7 + 8 =", 7+8)
+print("")
+print("9 + 3 = "+ str(9+3),"\n")
+
+print("""\n\n\n\nOhh my Grimas 
+Baby Gronk just rizzed up Livvy Dune""")
