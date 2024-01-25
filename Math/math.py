111990716d6e4ae1841b3008cad47c9fd55eb5bb
Sullivan <sullivanabegg8@gmail.com>
2024-01-25 09:03:03 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+#Programmer: Sullivan Abegg
+#Program: Math
+#Date: 1.10.2024
+
+print(9 * 9)
+print(9 + 9)
+print(9 - 6)
+print(9 / 3)
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+print(50 ** 2) #Exsponent
+print(50 % 6) #Modulo - what is left over after divsion (Remainder)
+print(50 / 6) #Divsion with remainder
+print(50 // 6) #whole number with no remainder
